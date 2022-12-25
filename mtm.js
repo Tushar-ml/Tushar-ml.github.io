20557cb8c2961c168abe0b59af55179bc56349c7
Tushar Goel <tushar.goel.ml@gmail.com>
2022-12-25 11:30:05 +0530

    optimized images and content

--- mtm.js
+++
@@ -1,8 +0,0 @@
-const mediumToMarkdown = require('medium-to-markdown');
-
-var url = 'https://medium.com/@tushargoel-ml/near-real-time-optimization-of-notifications-at-linkedin-part-i-893dcb8eef41'
-// Enter url here
-mediumToMarkdown.convertFromUrl(url)
-.then(function (markdown) {
-  console.log(markdown); //=> Markdown content of medium post
-});
